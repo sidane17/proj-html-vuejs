666aa029c1b1f92ba81d49ff98269a1bdacffade
sidane17 <khalilsidane89@gmail.com>
2024-06-04 15:25:33 +0200

    rifinitura slider

--- src/components/appMainUno.vue
+++ src/components/appMainUno.vue
@@ -1,6 +1,14 @@
 <script >
-
-</script>
+// import store from "../"
+// export default{
+//     data(){
+//         return{
+//             store
+//         }
+//     },
+  
+// }
+// </script>
 
 
 <template>
@@ -9,17 +17,17 @@
             <div class="card_squad col-2"></div>
         </div>
         <div class="sub_container w-100 mt-5 d-flex">
-            <!-- sezione uno-uno -->
+            <!-- sezione UNO-uno -->
             <div class="col-6 border d-flex justify-content-center align-items-center">
                 <div class="container_img border d-flex justify-content-center align-items-center">
                     <div class="d-flex justify-content-center align-items-center" style="z-index: 1;">
-                        <img src="../assets/2.png" alt="" width="20rem" style="aspect-ratio: 1/1;">
+                        <img src="../assets/exprince.jpg" alt="" width="350px" style="aspect-ratio: 1/1;">
                     </div>
-                    <div class="align-self-start justify-self-start"><img src="../assets/shape2.png" alt="" width="100%"></div>
-                    <div class="align-self-end"><img src="../assets/shape1.png" alt="" width="100%" style="z-index: 0;"></div>
+                    <div class="align-self-start justify-self-start sCube"><img src="../assets/shape2.png" alt="" width="200px"></div>
+                    <div class="align-self-end bCube"><img src="../assets/shape1.png" alt="" width="100px" style="z-index: 0;"></div>
                 </div>
             </div>
-            <!-- sezioneuno-due -->
+            <!-- sezione UNO-due -->
             <div class="col-6 border d-flex justify-content-center align-items-center flex-column">
                 <p>12 Year Experience</p>
                 <h2> We are the best Gaming Company</h2>
@@ -37,9 +45,98 @@
             </div>
         </div>
     </div>
+    <!-- sezione DUE -->
+    <div class="container_sezione2 ">
+        <div class="container d-flex flex-wrap">
+        <div class="col-6 d-flex flex-column text-light justify-content-center " >
+            <p class="text-success"><b> Our Extreme Feature</b></p>
+            <h2 class="mb-4 fs-1"><b>The best gaming arena on the World</b></h2>
+            <p class="fs-5">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore, voluptatem. Dolore ipsa eligendi, veritatis aspernatur cumque, pariatur accusamus voluptatem ducimus voluptates molestiae ut quod iure unde consequatur ad optio vero?</p>
 
+        </div>
+        <div class="col-6 d-flex flex-wrap">
+            <div class="cardGame d-flex align-items-center justify-content-center flex-column m-2 rounded">
+                <div class="circle d-flex justify-content-center align-items-center flex-column">
+                    <img src="../assets/feature1.png" alt="" width="75px" height="75px">
+                    
+                </div>
+                <span class=""><b>Live Streaming</b></span>
+                <p class="text-center">Nulla porttitor accusuam tincidunt.Pellentesque in.</p>
+            </div>
+            <div class="cardGame d-flex align-items-center justify-content-center flex-column m-2 rounded">
+                <div class="circle d-flex justify-content-center align-items-center flex-column">
+                    <img src="../assets/feature2.png" alt="" width="75px" height="75px">
+                    
+                </div>
+                <span class=""><b>Gaming News</b></span>
+                <p class="text-center">Nulla porttitor accusuam tincidunt.Pellentesque in.</p>
+            </div>
+            <div class="cardGame d-flex align-items-center justify-content-center flex-column m-2 rounded"  >
+                <div class="circle d-flex justify-content-center align-items-center flex-column">
+                    <img src="../assets/feature3.png" alt="" width="75px" height="75px">
+                    
+                </div>
+                <span class=""><b>Great Tournament</b></span>
+                <p class="text-center">Nulla porttitor accusuam tincidunt.Pellentesque in.</p>
+            </div>
+            <div class="cardGame d-flex align-items-center justify-content-center flex-column m-2 rounded">
+                <div class="circle d-flex justify-content-center align-items-center flex-column">
+                    <img src="../assets/feature4.png" alt="" width="75px" height="75px">
+                    
+                </div>
+                <span class=""><b>Awward Ceremony</b></span>
+                <p class="text-center">Nulla porttitor accusuam tincidunt.Pellentesque in.</p>
+            </div>
+        </div>
+    </div>
+    </div>
+    <!-- sezione TRE -->
+    <div class="container_sezione3">
+        <div class="container d-flex">
+            <div class="containerVideo col-6 d-flex flex-column justify-content-center align-items-center  ">
+                <div class="play p-4 bg-">
+                    <i class="fa-solid fa-play text-light fs-4"></i>
+                </div>
+                <p class="text-light mt-2"><b>Call Of Deauty</b></p>
+            </div>
+            <div class="col-6 d-flex justify-content-center ms-4 flex-column">
+                <p class="text-success fs-5"><b>Live Streaming</b></p>
+                <h2 class="text-light ">Checkout Our Live Streaming</h2>
+                <div class="containerCarousel">
+                    <!-- carousel -->
+
+                    <div id="carouselExample" class="carousel slide" style="height: 100%;">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                            <img src="../assets/v-small2.png" class="h " alt="">
+                            </div>
+                            <div class="carousel-item ">
+                            <img src="../assets/v-small1.png" class="h " alt="">
+                            </div>
+                            
+                        </div>
+                            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                            </button>
+                            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                            </button>
+                        </div>
+
+                     
+                </div> 
+                
+            </div>
+        </div>
+    </div>
 </template>
 <style>
+.h{
+    width:20rem ;
+    border-radius:20px
+}
 li{
     list-style: none;
 }
@@ -82,5 +179,76 @@ li{
     aspect-ratio: 1/1;
     height: 90%;
     position: relative;
+}
+.bCube{
+    position: absolute;
+    top: 0;
+    left: 0;
+}
+.sCube{
+    position: absolute;
+    right: 0;
+    bottom: 0;
+}
+/* SEZIONE 2  */
+.container_sezione2{
+    height: 100vh;
+    width: 100%;
+    background-image: url(../assets/Vector.png);
+    background-color: rgb(47, 47, 86) ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+}
+.cardGame{
+    width: 17rem ;
+    aspect-ratio: 1/1;
+    
+    background-color: rgb(111, 229, 65);
+    
+    
+}
+.circle{
+    width: 7rem;
+    aspect-ratio: 1/1;
+    border-radius: 50%;
+    background-color: rgb(55, 55, 89);
+}
+/* sezione TRE */
+.container_sezione3 {
+    height: 100vh;
+    width: 100%;
+    background-image: url(../assets/v-bg.png);
+    background-color: rgb(47, 47, 86) ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.containerVideo{
+    aspect-ratio: 4/3;
+    background-image: url(../assets/v-1.png);
+    background-size: cover ;
+    background-position: center;
+    border-radius: 20px;
+    
+}
+.containerSlider{
+    aspect-ratio: 4/3;
+    border-style: dashed;
+}
+.play{
+    aspect-ratio: 1/1;
+    width: 5rem;
+    background-color: rgb(43, 136, 68);
+    border-radius: 50% ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.containerCarousel{
+    height: 15rem;
+    width: 50%;
+    border-style:dashed;
 }
 </style>
